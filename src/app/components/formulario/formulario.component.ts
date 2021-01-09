c98d18b53c4b756bde40dd1e90153507f3ff6f16
unknown <daciros3284@gmail.com>
2021-01-08 19:50:29 -0500

    estilos y formulario sin funcion

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { from } from 'rxjs';
+import {GlobalsService} from'../../services/global/globals.service';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+
+ // form: FormGroup;
+ // global: GlobalsService ;
+
+  constructor() {
+    //this.buildForm();
+    
+   }
 
   ngOnInit(): void {
   }
+/*
+  public buildForm (){
+    this.form = new FormGroup({
+        name: new FormControl(''),
+        document: new FormControl(this.global.Enmascara),
+        key: new FormControl(this.global.Keylock)
+    });
+  }
+*/
 
 }
